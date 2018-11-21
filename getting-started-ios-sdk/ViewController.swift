fa4e5b7f1a90b79583a1cc23b5305bb0942ece09
Jeremy Zhang <jeremy.zhang@smartcar.com>
2018-11-21 14:25:56 -0800

    Display vehicle info onscreen

--- getting-started-ios-sdk/ViewController.swift
+++ getting-started-ios-sdk/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController {
     
     let appDelegate = UIApplication.shared.delegate as! AppDelegate
     @IBOutlet weak var button: UIButton!
-
+    @IBOutlet weak var vehicle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.vehicle.adjustsFontSizeToFitWidth = true
         
         let clientId = "29123987-3302-4145-8422-5bfff70b7d89"
         
@@ -39,6 +42,18 @@ class ViewController: UIViewController {
         Alamofire.request("http://localhost:8000/callback?code=" + code!, method: .get).responseJSON {_ in
             Alamofire.request("http://localhost:8000/vehicle", method: .get).responseJSON { response in
                 print(response.result.value!)
+                
+                if let result = response.result.value {
+                    let JSON = result as! NSDictionary
+                    print(JSON.object(forKey: "make"))
+                    
+                    let make = JSON.object(forKey: "make")!  as! String
+                    let model = JSON.object(forKey: "model")!  as! String
+                    let year = String(JSON.object(forKey: "year")!  as! Int)
+                    
+                    let vehicle = year + " " + make + " " + model;
+                    
+                    self.vehicle.text = vehicle                }
             
                 self.viewDidAppear(_: true)
             }
